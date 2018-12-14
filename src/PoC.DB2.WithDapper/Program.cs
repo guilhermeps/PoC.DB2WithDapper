bcb6ef02653ff266002791c1028fba506918bc94
Guilherme de Paula Silva <guilhermeps@ciandt.com>
2018-12-14 17:33:47 -0200

    Initial commit.

---
+++ src/PoC.DB2.WithDapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoC.DB2.WithDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tst/PoC.DB2WithDapper.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PoC.DB2WithDapper.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
